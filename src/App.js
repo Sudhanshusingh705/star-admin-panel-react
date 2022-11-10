398daddc1024e01cf9317ae2e16ba0ade3e3a473
Sudhanshusingh705 <Sudhanshusingh705495@gmail.com>
2022-11-10 21:41:58 +0530

    .....

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Settingpanel from "./Components/Settingpanel";
 import Slidebar from "./Components/Slidebar";
+import chart from "./Components/chart";
 import Footer from "./Components/Footer";
 
 
