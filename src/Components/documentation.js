27f7781493079b517d52af35b7daa4c7ce62728c
Sudhanshusingh705 <Sudhanshusingh705495@gmail.com>
2022-11-10 22:34:56 +0530

    ..

--- src/Components/documentation.js
+++ src/Components/documentation.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 const documentation = () => {
   return (
     <div>
-     
         {/* Required meta tags */}
         <meta charSet="utf-8" />
         <meta
